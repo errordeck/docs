b4857ae1a78cf910fa0571eede578e7e0a8668a3
Håkan <hakan@dun.se>
2023-06-25 15:30:46 +0200

    updated social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,7 +53,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/social_card.png',
       navbar: {
         title: 'Errordeck Docs',
         logo: {
